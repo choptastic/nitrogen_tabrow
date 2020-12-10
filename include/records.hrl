fc404f6ede4687157925d3544dd9d7c191b4af19
Jesse Gumm <gumm@sigma-star.com>
2020-12-10 15:43:22 -0600

    Lay out the element definition

--- include/records.hrl
+++ include/records.hrl
@@ -1,1 +1,13 @@
--record(tabrow, {?ELEMENT_BASE(element_tabrow), tabs=[], tag, bodies=[]}).
+-record(tabrow_item, {
+    id              :: id(),
+    tag             :: term(),
+    text            :: text(),
+    body            :: body(),
+    selected=false  :: boolean()
+}.
+
+-record(tabrow, {?ELEMENT_BASE(element_tabrow),
+    tabs=[]         :: [#tabrow_item{} | '|'],
+    tag             :: term(),
+    selected        :: term()
+}).
