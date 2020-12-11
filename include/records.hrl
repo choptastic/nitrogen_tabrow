137acb4619060bbd3376bb63ff84b36a7cd13ec4
Jesse Gumm <gumm@sigma-star.com>
2020-12-10 18:06:47 -0600

    fix records. add orientation

--- include/records.hrl
+++ include/records.hrl
@@ -4,10 +4,11 @@
     text            :: text(),
     body            :: body(),
     selected=false  :: boolean()
-}.
+}).
 
 -record(tabrow, {?ELEMENT_BASE(element_tabrow),
     tabs=[]         :: [#tabrow_item{} | '|'],
     tag             :: term(),
-    selected        :: term()
+    selected        :: term(),
+    orientation=top :: top | right | bottom | left
 }).
