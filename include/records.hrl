c35875538693e075884481e484b663c4af3a0c64
Jesse Gumm <gumm@sigma-star.com>
2021-02-10 19:27:19 -0600

    flesh out

--- include/records.hrl
+++ include/records.hrl
@@ -1,14 +1,16 @@
--record(tabrow_item, {
+-record(tabrow_tab, {
+    class           :: class(),
     id              :: id(),
     tag             :: term(),
     text            :: text(),
-    body            :: body(),
-    selected=false  :: boolean()
+    body            :: body()
 }).
 
 -record(tabrow, {?ELEMENT_BASE(element_tabrow),
-    tabs=[]         :: [#tabrow_item{} | '|'],
+    tabs=[]         :: [#tabrow_tab{} | '|'],
     tag             :: term(),
-    selected        :: term(),
+    selected        :: id(),
+    target          :: id(),
+    mode            :: tabs | pills,
     orientation=top :: top | right | bottom | left
 }).
--- src/element_tabrow.erl
+++ src/element_tabrow.erl
@@ -8,11 +8,61 @@
     event/1
 ]).
 
+
+-define(SELECTED, tabrow_selected).
+
 reflect() -> record_info(fields, tabrow).
 
-render_element(Rec = #tabrow{}) ->
-    [].
+render_element(#tabrow{
+        class=Class,
+        tabs=Tabs,
+        tag=TabrowTag,
+        selected=Selected,
+        target=Targetid,
+        mode=Mode,
+        orientation=Orientation}) ->
+    Wrapperid = wf:temp_id(),
+
+    OrientationClass = "tabrow_" ++ wf:to_list(Orientation),
+    ModeClass = "tabrow_" ++ wf:to_list(Mode),
+
+    #panel{id=Wrapperid, class=[tabrow, Class, ModeClass, OrientationClass], body=[
+        [draw_tab(Wrapperid, Targetid, TabrowTag, Selected, Tab) || Tab <- Tabs]
+    ]}.
+
+draw_tab(Wrapperid, Targetid, TabrowTag, Selected, #tabrow_tab{
+                                                    id=Tabid,
+                                                    tag=TabTag,
+                                                    text=Text,
+                                                    body=Body,
+                                                    class=Class
+                                                }) ->
+
+    TabidClass = ?WF_IF(Tabid=="", wf:normalize_id("tabrow_blank"), wf:normalize_id(Tabid)),
+    SelectedClass = ?WF_IF(wf:to_list(Selected)==wf:to_list(Tabid), ?SELECTED),
+    #link{
+        class=[tabrow_tab, TabidClass, SelectedClass, Class],
+        text=Text,
+        body=Body,
+        postback={tab, Wrapperid, Targetid, Tabid, TabrowTag, TabTag},
+        delegate=?MODULE
+    }.
 
 
-event({tab, Tab}) ->
-    [].
+event({tab, Wrapperid, Targetid, Tabid, TabrowTag, TabTag}) ->
+    Mod = wf:page_module(),
+    case Mod:tabrow_event(TabrowTag, TabTag) of
+        {ok, Body} -> 
+            wf:defer(".wfid_" ++ Wrapperid ++ " .tabrow_tab", #remove_class{class=tabrow_selected}),
+            wf:defer(Tabid, #add_class{class=tabrow_selected}),
+            wf:update(Targetid, Body),
+            ok;
+        ok -> 
+            wf:defer(".wfid_" ++ Wrapperid ++ " .tabrow_tab", #remove_class{class=tabrow_selected}),
+            wf:defer(Tabid, #add_class{class=tabrow_selected}),
+            ok;
+        Result ->
+            error_logger:warning_msg("Failed tabrow_event:~nTabrowTag: ~p~nTabTag: ~p~n~nResult from tabrow_event: ~p~n",[TabrowTag, TabTag, Result])
+   end.
+
+    
